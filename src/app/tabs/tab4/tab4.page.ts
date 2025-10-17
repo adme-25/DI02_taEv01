3e4eaefa2c14b955207c40b9f2ed72ee25e3caed
anaDavila <adavila@birt.eus>
2025-10-17 15:02:22 +0200

    terminado

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
-import { booleanAttribute, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -7,33 +8,56 @@ import { booleanAttribute, Component, OnInit } from '@angular/core';
   standalone: false,
 })
 
-
-
 export class Tab4Page implements OnInit{
   numAdivinar: number = 0;
   numPropuesto: any;
   mensaje: string = "Introduce un número entre 0 y 100";
   contador: number = 0;
-  constructor() { }
+  reiniciar: boolean = false;
+
+  constructor(private alerta: AlertController) { }
 
   onClick() {
-    console.log("botón pulsado");
-    this.contador = 1 + this.contador;
-    console.log("contador: " + this.contador);
+    this.contador++;
+    
     if (this.numPropuesto > this.numAdivinar) {
       this.mensaje="Introduce un número menor";
     } else if (this.numPropuesto < this.numAdivinar) {
       this.mensaje="Introduce un número mayor";
-    } else if (this.numPropuesto = this.numAdivinar) {
+    } else if (this.numPropuesto === this.numAdivinar) {
       this.mensaje="Has acertado";
+      this.reiniciar = true;
     }
   }
-  jugarOtravez() {}
 
   ngOnInit(): void {
     this.numAdivinar = Math.floor(Math.random()*100 + 1);
     console.log("El numero a adivinar es: " + this.numAdivinar);
   }
 
+  async jugarOtravez() {
+    const alert = await this.alerta.create({
+      header: 'Jugar de nuevo',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {}
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            this.ngOnInit();
+            this.numPropuesto = null;
+            this.contador =0;
+            this.mensaje = "Introduce un número entre 0 y 100";
+            this.reiniciar = false;
+          } 
+        }
+      ]
+    });
+    await alert.present();
+  }
 
 }
